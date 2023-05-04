7c6d86c02ef2d391177a90945d5f34d24017e404
shariful islam <sharifulb07@gmail.com>
2023-05-04 19:07:48 +0600

    done development of food restaurant landing pages

--- script.js
+++ script.js
@@ -3,3 +3,60 @@ const header=document.querySelector('header');
 window.addEventListener('scroll',function(){
     header.classList.toggle("sticky", window.scrollY>80);
 })
+
+
+
+
+let menu=document.querySelector('.bx-menu');
+let navlist=document.querySelector('.navlist');
+
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+
+
+
+
+
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'85px',
+    duration:2500,
+    reset:true
+})
+
+
+
+
+
+
+sr.reveal('.home-text', {delay:100});
+sr.reveal('.home-img', {delay:600});
+sr.reveal('.about-img', {});
+sr.reveal('.about-text', {delay:300});
+sr.reveal('.middle-text', {});
+sr.reveal('.row-btn .shop-content', {delay:300});
+
+
+
+const sr1=ScrollReveal({
+    origin:'bottom',
+    distance:'85px',
+    duration:2500,
+    reset:true
+})
+
+
+sr1.reveal('.container-box ', {delay:800});
+
+
+
+
